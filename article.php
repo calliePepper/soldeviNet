f38c87134299462187ce1cc04a8f75233e8c07c4
soldeviFae <faedaunt@gmail.com>
2015-09-29 10:39:43 +1000

    Tagline and include function
    The tagline should now be on all pages, as well as a fix to the include
    functionality and a bugfix for masonry

--- article.php
+++ article.php
@@ -27,43 +27,10 @@
 			$innerQuery->fetch(); 
 		}
 	}
+	$bodyId = "homePage";
+	$titleInfo = $title;
+	include "includes/header.php";
 ?>
-<!DOCTYPE html>
-<html> 
-    <head> 
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1">   
-        <title>Soldevi - <?=$title?></title>
-        <link href="/css/style.css" rel="stylesheet" type="text/css" />
-        <link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,600' rel='stylesheet' type='text/css'>
-        <script type="text/javascript" src="/script/jquery.min.js"></script>
-        <script type="text/javascript" src="/script/shared.js"></script>
-    </head>
-    <body id="homePage">
-	    <header>
-	    	<div class="generalHeader clearFloat">
-	    		<div class="marginedIn">
-		    		<!--<img src="" class="logo" />-->
-			    	<a class="logoLink" href="/index.php">
-			    		<span class="logo">
-			    			Soldevi
-			    		</span>
-			    	</a>
-			    	<div class="search">
-			    		<input type="text" class="searchBox" name="searchBox" id="searchBox" />
-			    	</div>
-			    	<span class="clearFloatMobile"></span>
-		    		<nav>
-		    			<ul>
-		    				<li><a href="/index.php" class="navigationLink homeLink">Home</a></li>
-		    				<li><a href="/archive.php" class="navigationLink archiveLink">Archive</a></li>
-		    				<li><a href="/students.php" class="navigationLink studentLink">Students</a></li>
-		    				<li><a href="/about.php" class="navigationLink aboutLink">About</a></li>
-		    			</ul>
-		    		</nav>
-		    	</div>
-	    	</div>
 	    </header>
 	    <div class="blogImageLarge" style="background-image:url('/img/<?=$imageURL?>')"></div>
 	    <div class="topicBar marginedIn"></div>
