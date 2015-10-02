28014c14c073f236646075afa85e2087507b6e3d
soldeviFae <faedaunt@gmail.com>
2015-10-02 11:20:48 +1000

    UGH GITHUB

--- students.php
+++ students.php
@@ -1,39 +1,8 @@
-<!DOCTYPE html>
-<html> 
-    <head> 
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1">   
-        <title>Soldevi - My students</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css" />
-        <link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,600' rel='stylesheet' type='text/css'>
-        <script type="text/javascript" src="script/jquery.min.js"></script>
-        <script type="text/javascript" src="script/shared.js"></script>
-    </head>
-    <body id="studentPage">
-	    <header>
-	    	<div class="generalHeader clearFloat">
-	    		<div class="marginedIn">
-		    		<!--<img src="" class="logo" />-->
-			    	<a class="logoLink" href="index.php">
-			    		<span class="logo">
-			    			Soldevi
-			    		</span>
-			    	</a>
-			    	<div class="search">
-			    		<input type="text" class="searchBox" name="searchBox" id="searchBox" />
-			    	</div>
-			    	<span class="clearFloatMobile"></span>
-		    		<nav>
-		    			<ul>
-		    				<li><a href="index.php" class="navigationLink homeLink">Home</a></li>
-		    				<li><a href="archive.php" class="navigationLink archiveLink">Archive</a></li>
-		    				<li><a href="students.php" class="navigationLink studentLink">Students</a></li>
-		    				<li><a href="about.php" class="navigationLink aboutLink">About</a></li>
-		    			</ul>
-		    		</nav>
-		    	</div>
-	    	</div>
+<?php
+	$bodyId = "studentPage";
+	$titleInfo = "Current and Past Students";
+	include "includes/header.php";
+?>
 	    	<div class="hero"></div>
 	    	<div class="topicBar marginedIn"></div>
 	    </header>
@@ -50,6 +19,7 @@
 		    		<h2>Current Students</h2>
 		    		<div class="student"><strong>Stuart Mitchelhill</strong> - <a href="http://stumitchdesign.com">stumitchdesign.com</a></div>
 		    		<div class="student"><strong>Maryo Sumenge</strong> - <a href="http://www.maryo.info">www.maryo.info</a></div>
+		    		<div class="student"><strong>Jahl Herapath</strong> - <a href="http://www.ohhcdd.com">www.ohhcdd.com</a></div>
 		    	</section>
 		    </div>
 	    </div>
